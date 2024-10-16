09ec7276cdd44bc95df5b094ab36a5d755c34f9e
regina-massarova <reg.massarova@gmail.com>
2024-10-16 20:56:18 +0400

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,28 +9,28 @@ class MainPage(BasePage):
         super().__init__(driver)
         self.driver.get(self.URL)
 
-    @allure.step('Кликаем на вопрос')
+    @allure.step('Клик на вопрос')
     def click_to_question(self, num):
         locator_q_formatted = self.format_locator(MainPageLocators.QUESTION_LOCATOR, num)
         locator_8_formatted = self.format_locator(MainPageLocators.QUESTION_LOCATOR_8, 7)
         self.scroll_to_element(locator_8_formatted)
         self.click_to_element(locator_q_formatted)
 
-    @allure.step('Получаем текст с ответа')
+    @allure.step('Получаем текст ответа')
     def get_answer_text(self, num):
         locator_a_formatted = self.format_locator(MainPageLocators.ANSWER_LOCATOR, num)
         self.click_to_question(num)
         return self.get_text_from_element(locator_a_formatted)
 
-    @allure.step('Нажимаем на кнопку принятия куков')
+    @allure.step('Клик на кнопку принять cookie')
     def click_cookie_button(self):
         self.click_to_element(MainPageLocators.COOKIE_BUTTON)
 
-    @allure.step('Нажимаем на верхнюю кнопку заказать')
+    @allure.step('Клик на кнопку заказать вверху')
     def click_order_button_top(self):
         self.click_to_element(MainPageLocators.ORDER_BUTTON_TOP_LOCATOR)
 
-    @allure.step('Нажимаем на нижнюю кнопку заказать')
+    @allure.step('Клик на кнопку заказать внизу')
     def click_order_button_bottom(self):
         self.scroll_to_element(MainPageLocators.ORDER_BUTTON_BOTTOM_LOCATOR)
         self.click_to_element(MainPageLocators.ORDER_BUTTON_BOTTOM_LOCATOR)
