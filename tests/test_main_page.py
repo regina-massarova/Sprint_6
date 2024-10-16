4d480750191fa9e2fb4740e36fa4baa01e265754
regina-massarova <reg.massarova@gmail.com>
2024-10-16 21:00:29 +0400

    Update test_main_page.py

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -7,12 +7,12 @@ from data import ANSWERS
 
 class TestMainPage:
 
-    @allure.description('Проверяем, что при клике на вопрос ответ правильный')
+    @allure.description('Проверяем, что при клике на вопрос ответ верный')
     @pytest.mark.parametrize(
         'num, result',
-        [(i, ANSWERS[i]) for i in range(8)]  # Параметризуем тест для всех вопросов
+        [(i, ANSWERS[i]) for i in range(8)]  
     )
     def test_questions_and_answers(self, driver, num, result):
         main_page = MainPage(driver)
-        assert main_page.get_answer_text(num) == result  # Проверяем, что текст ответа соответствует ожиданиям
+        assert main_page.get_answer_text(num) == result  
 
