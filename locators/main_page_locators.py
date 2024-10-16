53365e67e5feba4a46dcf1e76b9c569e441dfcd2
regina-massarova <reg.massarova@gmail.com>
2024-10-16 20:53:43 +0400

    Update main_page_locators.py

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -2,10 +2,10 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     COOKIE_BUTTON = (By.ID, "rcc-confirm-button")  # Локатор кнопки куков
-    QUESTION_LOCATOR = By.XPATH, '//*[@id="accordion__heading-{}"]'  # Локатор для вопросов
-    QUESTION_LOCATOR_8 = By.XPATH, '//*[@id="accordion__heading-7"]'  # Локатор для 8-го вопроса (для скроллинга)
-    ANSWER_LOCATOR = By.XPATH, '//*[@id="accordion__panel-{}"]'  # Локатор для ответов
-    ORDER_BUTTON_TOP_LOCATOR = By.XPATH, '//button[contains(@class, "Button_Button__ra12g") and not(contains(@class, "Button_Middle__1CSJM"))]'  # Локатор верхней кнопки Заказать
-    ORDER_BUTTON_BOTTOM_LOCATOR = By.XPATH, '//button[contains(@class, "Button_Button__ra12g") and contains(@class, "Button_Middle__1CSJM")]'  # Локатор нижней кнопки Заказать
+    QUESTION_LOCATOR = By.XPATH, '//*[@id="accordion__heading-{}"]'  # Локатор вопросов
+    QUESTION_LOCATOR_8 = By.XPATH, '//*[@id="accordion__heading-7"]'  # Локатор 8-го вопроса (скроллинг)
+    ANSWER_LOCATOR = By.XPATH, '//*[@id="accordion__panel-{}"]'  # Локатор ответов
+    ORDER_BUTTON_TOP_LOCATOR = By.XPATH, '//button[contains(@class, "Button_Button__ra12g") and not(contains(@class, "Button_Middle__1CSJM"))]'  # Локатор кнопки Заказать вверху
+    ORDER_BUTTON_BOTTOM_LOCATOR = By.XPATH, '//button[contains(@class, "Button_Button__ra12g") and contains(@class, "Button_Middle__1CSJM")]'  # Локатор кнопки Заказать внизу
 
 
